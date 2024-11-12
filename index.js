e6c1b8968601cf7835f1d66d1665c23b19681a56
HenriqueMGA <riquemamaral@gmail.com>
2024-11-12 12:10:02 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 let nomeDoHeroi = "Henrique"
 let experienciaDoHeroi = Math.floor(Math.random() * (10001 - 1000) + 1000)
+let nivelDoHeroi = ""
 
 if (experienciaDoHeroi <= 1000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ferro`)
+    nivelDoHeroi = "Ferro"
 } else if (experienciaDoHeroi > 1000 && experienciaDoHeroi <= 2000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Bronze`)
+    nivelDoHeroi = "Bronze"
 } else if (experienciaDoHeroi > 2000 && experienciaDoHeroi <= 5000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Prata`)
+    nivelDoHeroi = "Prata"
 } else if (experienciaDoHeroi > 5000 && experienciaDoHeroi <= 7000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ouro`)
+    nivelDoHeroi = "Ouro"
 } else if (experienciaDoHeroi > 7000 && experienciaDoHeroi <= 8000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Platina`)
+    nivelDoHeroi = "Platina"
 } else if (experienciaDoHeroi > 8000 && experienciaDoHeroi <= 9000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ascendente`)
+    nivelDoHeroi = "Ascendente"
 } else if (experienciaDoHeroi > 9000 && experienciaDoHeroi <= 10000) {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Imortal`)
+    nivelDoHeroi = "Imortal"
 } else {
-    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Radiante`)
+    nivelDoHeroi = "Radiante"
 }
+
+console.log(`O Herói de nome: ${nomeDoHeroi} está no nível: ${nivelDoHeroi}`)
